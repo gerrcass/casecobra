170174ce06f142ad2f56b884489a7129b34a6f0c
Gerardo Castillo <gerrcass@gmail.com>
2024-07-18 18:36:52 -0400

    Fix type definitions for Stripe webhook endpoint.

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -4,6 +4,13 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
+interface ExtendedSession extends Stripe.Checkout.Session {
+  shipping?: {
+    address: Stripe.Address;
+    name?: string | null;
+  };
+}
+
 export async function POST(req: Request) {
   try {
     const signature = headers().get("stripe-signature");
@@ -23,7 +30,7 @@ export async function POST(req: Request) {
         throw new Error("Missing user email");
       }
 
-      const session = event.data.object as Stripe.Checkout.Session;
+      const session = event.data.object as ExtendedSession;
 
       const { userId, orderId } = session.metadata || {
         userId: null,
@@ -35,9 +42,9 @@ export async function POST(req: Request) {
       }
 
       const billingAddress = session.customer_details!.address;
-      const shippingAddress = session.shipping_details!.address;
+      const shippingAddress = session.shipping!.address;
 
-      const updatedOrder = await db.order.update({
+      await db.order.update({
         where: {
           id: orderId,
         },
@@ -77,3 +84,102 @@ export async function POST(req: Request) {
     );
   }
 }
+
+// const requestBodyExample = {
+//   id: "cs_test_a17mmM8HOuPL8iOOkgrKBD965CunYB99Hd9UsEn2IHF0e415sqAMyipE4S",
+//   object: "checkout.session",
+//   after_expiration: null,
+//   allow_promotion_codes: null,
+//   amount_subtotal: 2299,
+//   amount_total: 2299,
+//   automatic_tax: { enabled: false, liability: null, status: null },
+//   billing_address_collection: null,
+//   cancel_url:
+//     "http://localhost:3000/configure/preview?id=clyrsoo2c0000a20xdmkzdlo5",
+//   client_reference_id: null,
+//   client_secret: null,
+//   consent: null,
+//   consent_collection: null,
+//   created: 1721338875,
+//   currency: "usd",
+//   currency_conversion: null,
+//   custom_fields: [],
+//   custom_text: {
+//     after_submit: null,
+//     shipping_address: null,
+//     submit: null,
+//     terms_of_service_acceptance: null,
+//   },
+//   customer: null,
+//   customer_creation: "if_required",
+//   customer_details: {
+//     address: {
+//       city: "Thousand Oaks",
+//       country: "US",
+//       line1: "No street here2",
+//       line2: "My second line2",
+//       postal_code: "91360",
+//       state: "CA",
+//     },
+//     email: "me2@gerardo.com",
+//     name: "St Nobody2",
+//     phone: null,
+//     tax_exempt: "none",
+//     tax_ids: [],
+//   },
+//   customer_email: null,
+//   expires_at: 1721425275,
+//   invoice: null,
+//   invoice_creation: {
+//     enabled: false,
+//     invoice_data: {
+//       account_tax_ids: null,
+//       custom_fields: null,
+//       description: null,
+//       footer: null,
+//       issuer: null,
+//       metadata: {},
+//       rendering_options: null,
+//     },
+//   },
+//   livemode: false,
+//   locale: null,
+//   metadata: {
+//     orderId: "clyrsq00q0002a20x5sxkhi9h",
+//     userId: "kp_0f68a92f262c487899906c300a8526c6",
+//   },
+//   mode: "payment",
+//   payment_intent: "pi_3Pe25UGNhgkHb9n00y0Nby8Z",
+//   payment_link: null,
+//   payment_method_collection: "if_required",
+//   payment_method_configuration_details: null,
+//   payment_method_options: { card: { request_three_d_secure: "automatic" } },
+//   payment_method_types: ["card"],
+//   payment_status: "paid",
+//   phone_number_collection: { enabled: false },
+//   recovered_from: null,
+//   saved_payment_method_options: null,
+//   setup_intent: null,
+//   shipping: {
+//     address: {
+//       city: "Thousand Oaks",
+//       country: "US",
+//       line1: "No street here2",
+//       line2: "My second line2",
+//       postal_code: "91360",
+//       state: "CA",
+//     },
+//     name: "St Nobody2",
+//   },
+//   shipping_address_collection: { allowed_countries: ["DE", "US"] },
+//   shipping_options: [],
+//   shipping_rate: null,
+//   status: "complete",
+//   submit_type: null,
+//   subscription: null,
+//   success_url:
+//     "http://localhost:3000/thank-you?orderId=clyrsq00q0002a20x5sxkhi9h",
+//   total_details: { amount_discount: 0, amount_shipping: 0, amount_tax: 0 },
+//   ui_mode: "hosted",
+//   url: null,
+// };
